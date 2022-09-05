079dae0504bc9a8f580ca1f3ca1ddf76871390d0
readingwritingcode <david.medina1@udea.edu.co>
2022-09-05 11:33:26 -0500

    permananencia empresarialI ok.

--- reportP.py
+++ reportP.py
@@ -13,16 +13,21 @@ from pathlib import Path
 
 import pickle
 
-# Read parqueDB
-# pathlib!
-#lc_2014_2021.xlsx
-#dbs = Path('../dbs/')
-#lc_2014_2021 = dbs / 'lc_2014_2021.xlsx'
-# dff = pd.read_excel(lc_2014_2021.xlsx)
-dff = pd.read_excel('Listado_consolidado_ 2014_2021_V2 (5).xlsx')
+# variables 
+#format = {"EMPLEOS":"{:,.0f}", "EMPLEOS_x":"{:,.0f}", "EMPLEOS_y":"{:,.0f}", "VENTAS": "${:,.2f}", "VENTAS_IPC": "${:,.2f}", "VENT_PROM_IPC":"${:,.2f}", "PATENTES":"{:.0f}", "REGISTROS": "{:.0f}", "SECRETOS": "{:.0f}", "DERECHOS": "{:.0f}", "SECRETOS":"{:.0f}"}
 
-# Select some cols
-df = dff[['NOMBRE_EMPRENDIMIENTO', 'MES DEL ACOMPAÑAMIENTO REPORTADO',
+# read_db_parque
+def read_db(path_to_dbs,name_file):
+  '''read db excel file''' # enhance with multiple type of files!
+  dbs = Path(path_to_dbs)
+  file_db = dbs / name_file
+  return pd.read_excel(file_db)
+
+# transform df
+def preprocess_df(df):
+  '''transform dataframe''' # -> enhance documentation
+
+  df = df[['NOMBRE_EMPRENDIMIENTO', 'MES DEL ACOMPAÑAMIENTO REPORTADO',
        'VIGENCIA', 'MACROSECTOR','Institución emprendedor', '¿ESTA FORMALIZADA?',
        'NÚMERO DE EMPLEOS DIRECTOS GENERADOS AL MES',
        'TOTAL VENTAS MENSUALES EN COLOMBIA', 'NÚMERO DE PATENTES',
@@ -30,7 +35,7 @@ df = dff[['NOMBRE_EMPRENDIMIENTO', 'MES DEL ACOMPAÑAMIENTO REPORTADO',
        'NÚMERO DE SECRETOS INDUSTRIALES']]
 
 # Rename some cols
-df.rename(columns = {'NOMBRE_EMPRENDIMIENTO':'NOMBRE_EMPRENDIMIENTO',
+  df.rename(columns = {'NOMBRE_EMPRENDIMIENTO':'NOMBRE_EMPRENDIMIENTO',
                      'MES DEL ACOMPAÑAMIENTO REPORTADO':'MES',
                      'MACROSECTOR':'SECTOR',
                      'Institución emprendedor':'INSTITUCION',
@@ -43,12 +48,9 @@ df.rename(columns = {'NOMBRE_EMPRENDIMIENTO':'NOMBRE_EMPRENDIMIENTO',
                      'NÚMERO DE SECRETOS INDUSTRIALES':'SECRETOS'
                      },inplace=True)
 
-#! warning raised
+# Process some columns -> Month map equality
 
-# Process some columns
-# Month map equality
-
-month_map = {'ENERO': 1,
+  month_map = {'ENERO': 1,
              'FEBRERO':2,
              'MARZO':3,
              'ABRIL' :4,
@@ -63,73 +65,73 @@ month_map = {'ENERO': 1,
 
 # Money mapper by inflation
 # inflation: see: https://www.dineroeneltiempo.com/peso-colombiano/de-2021-a-valor-presente
-IPC= { 
- '2014' : 95.25,
- '2015'	: 100,
- '2016' :	107.51,
- '2017'	: 112.15,
- '2018' :	115.78,
- '2019' : 119.87,
- '2020' : 122.89,
- '2021'	: 127.19,
- '2022' : 138.73
-}
+  IPC= { 
+  '2014' : 95.25,
+  '2015'	: 100,
+  '2016' :	107.51,
+  '2017'	: 112.15,
+  '2018' :	115.78,
+  '2019' : 119.87,
+  '2020' : 122.89,
+  '2021'	: 127.19,
+  '2022' : 138.73
+  }
 
 # formater. global variable
-format = {"EMPLEOS":"{:,.0f}", "EMPLEOS_x":"{:,.0f}", "EMPLEOS_y":"{:,.0f}", "VENTAS": "${:,.2f}", "VENTAS_IPC": "${:,.2f}", "VENT_PROM_IPC":"${:,.2f}", "PATENTES":"{:.0f}", "REGISTROS": "{:.0f}", "SECRETOS": "{:.0f}", "DERECHOS": "{:.0f}", "SECRETOS":"{:.0f}"}
 
 # Data transform
-dfp['MES'] = dfp['MES'].apply(lambda x:unidecode.unidecode(x.upper()))
-dfp['MES_DATE'] = dfp['MES'].map(month_map)
-dfp['DATE'] = pd.to_datetime(dfp.apply(lambda row: str(int(row.VIGENCIA)) +'-'+ str(int(row.MES_DATE)), axis=1),format='%Y-%m-%d')
-dfp['NOMBRE_EMPRENDIMIENTO'] = dfp['NOMBRE_EMPRENDIMIENTO'].apply(lambda x: unidecode.unidecode(x.upper()))
-dfp['FORMALIZACION'] = dfp['FORMALIZACION'].fillna('NO')
-dfp['FORMALIZACION'] = dfp['FORMALIZACION'].apply(lambda x: unidecode.unidecode(x.upper()))
-dfp['SECTOR'] = dfp['SECTOR'].apply(lambda x: unidecode.unidecode(str(x))).str.upper()
-dfp['INSTITUCION'] = dfp['INSTITUCION'].apply(lambda x: unidecode.unidecode(str(x))).str.upper()
-dfp['EMPLEOS'] = dfp['EMPLEOS'].fillna(0)
-dfp['VIGENCIA'] = dfp['DATE'].dt.year
-dfp['VENTAS_IPC'] = dfp.apply(lambda row: row.VENTAS*(IPC['2022']/IPC[str(row.VIGENCIA)]),axis=1)
+  df['MES'] = df['MES'].apply(lambda x:unidecode.unidecode(x.upper()))
+  df['MES_DATE'] = df['MES'].map(month_map)
+  df['DATE'] = pd.to_datetime(df.apply(lambda row: str(int(row.VIGENCIA)) +'-'+ str(int(row.MES_DATE)), axis=1),format='%Y-%m-%d')
+  df['NOMBRE_EMPRENDIMIENTO'] = df['NOMBRE_EMPRENDIMIENTO'].apply(lambda x: unidecode.unidecode(x.upper()))
+  df['FORMALIZACION'] = df['FORMALIZACION'].fillna('NO')
+  df['FORMALIZACION'] = df['FORMALIZACION'].apply(lambda x: unidecode.unidecode(x.upper()))
+  df['SECTOR'] = df['SECTOR'].apply(lambda x: unidecode.unidecode(str(x))).str.upper()
+  df['INSTITUCION'] = df['INSTITUCION'].apply(lambda x: unidecode.unidecode(str(x))).str.upper()
+  df['EMPLEOS'] = df['EMPLEOS'].fillna(0)
+  df['VIGENCIA'] = df['DATE'].dt.year
+  df['VENTAS_IPC'] = df.apply(lambda row: row.VENTAS*(IPC['2022']/IPC[str(row.VIGENCIA)]),axis=1)
 
-# return df
+  return df
 
 # permanencia empresarialI
-names = df['NOMBRE_EMPRENDIMIENTO'].unique()
-dflbp = pd.DataFrame()
-for i in names: 
-  # min month
-  a = df[(df['NOMBRE_EMPRENDIMIENTO'] == i)]['MES_DATE'].min()
+def permanencia_I(df):
+  '''permanencia empresarial I''' # enhance with more parameters
+  names = df['NOMBRE_EMPRENDIMIENTO'].unique()
+  dflbp = pd.DataFrame()
+  for i in names: 
+    # min month
+    a = df[(df['NOMBRE_EMPRENDIMIENTO'] == i)]['MES_DATE'].min()
 
-  b = df[(df['NOMBRE_EMPRENDIMIENTO'] == i)]['MES_DATE'].max()
+    b = df[(df['NOMBRE_EMPRENDIMIENTO'] == i)]['MES_DATE'].max()
 
-  #print(a,'-----',b)
+    #print(a,'-----',b)
 
    # row
-  row_df = df[(dfp['NOMBRE_EMPRENDIMIENTO'] == i) & (df['MES_DATE'] == a)] #['FORMALIZACION']
+    row_df = df[(df['NOMBRE_EMPRENDIMIENTO'] == i) & (df['MES_DATE'] == a)] #['FORMALIZACION']
   
   # EFIA
-  efia =  row_df['FORMALIZACION'].values[0]
+    efia =  row_df['FORMALIZACION'].values[0]
 
-  row_df['EFIA'] =efia
+    row_df['EFIA'] =efia
  
-  # EFFA
-  effa = df[(df['NOMBRE_EMPRENDIMIENTO'] == i) & (df['MES_DATE'] == b)]['FORMALIZACION'].values[0] #['FORMALIZACION']
+    # EFFA
+    effa = df[(df['NOMBRE_EMPRENDIMIENTO'] == i) & (df['MES_DATE'] == b)]['FORMALIZACION'].values[0] #['FORMALIZACION']
   
-  row_df['EFFA'] = effa
+    row_df['EFFA'] = effa
 
-  if (efia == "NO" and effa =="SI"):
-    row_df["NOSI"] = 'SI'
-  else:
-    row_df["NOSI"] = 'NO'
+    if (efia == "NO" and effa =="SI"):
+      row_df["NOSI"] = 'SI'
+    else:
+      row_df["NOSI"] = 'NO'
 
 #  if (efia == "SI" and effa =="SI"):
 #   row_df["SISI"] = 'SI'
 #  else:
 #    row_df["SISI"] = 'NO'
 
-dflbp = dflbp.append(row_df)
-
-# return dflbp 
+    dflbp = dflbp.append(row_df)
+  return dflbp 
 
 # permanenciaII
 # read db rues
